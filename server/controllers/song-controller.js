9775459530e11bb8d6acd0176981c1d5841c6bc0
thwilk <135527401+thwilk@users.noreply.github.com>
2025-12-01 14:23:11 -0500

    add documentation

--- server/controllers/song-controller.js
+++ server/controllers/song-controller.js
@@ -2,20 +2,30 @@ const auth = require('../auth');
 const songdb = require('../db/songdb');
 
 
+// params = id, body = song update
+// return updated song
 const updateSong = async (req, res) => {}
-const getSongPairs = async (req, res) => {}
+
+// return all songs
+const getSongs = async (req, res) => {}
+
+// params = id
+// return song with id = id
 const getSong = async (req, res) => {}
+
+// params = id
+// deletes song (200 if ok)
 const deleteSong = async (req, res) => {}
+
+// params = id, body = song update
+// returns the newest song 
 const createSong = async (req, res) => {}
-
-
-
 
 
 module.exports = {
     createSong,
     deleteSong,
     getSong,
-    getSongPairs,
+    getSongs,
     updateSong
 } 
--- server/routes/song-router.js
+++ server/routes/song-router.js
@@ -4,13 +4,10 @@ const { song } = require('../controllers/index.js')
 const router = express.Router()
 const auth = require('../auth')
 
-router.get('/songpairs', auth.verify, song.getSongPairs)
+router.get('/songpairs', auth.verify, song.getSongPair)
 router.patch('/song/:id', auth.verify, song.updateSong)
 router.get('/song/:id', auth.verify, song.getSong)
 router.delete('/song/:id', auth.verify, song.deleteSong)
 router.post('/song', auth.verify, song.createSong)
-
-
-
 
 module.exports = router;
